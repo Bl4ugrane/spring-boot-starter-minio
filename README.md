35dbdc5ae70e47a20fb76f3c32ca6694005fa8ba
Угачёв Дмитрий <d.ugachev@dnx.team>
2021-12-20 16:38:55 +0700

    Добавил описание проекта, добавил новые методы

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
 # Spring Boot Starter MinIO
+
+Библиотека для взаимодействия с файловым хранилищем [MinIO](https://docs.min.io/docs/minio-quickstart-guide.html)
+
+### При использовании стартера, необходимо в файле настроек указать:
+
+```yaml
+  minio:
+    url: MINIO_URL
+    access-key: MINIO_ACCESS_KEY
+    secret-key: MINIO_SECRET_KEY
+```
+
+### Пример использования:
+
+```kotlin
+/**
+ * Сервис для извлечение файлов из файлового хранилища
+ */
+@Service
+class FileStorageService(
+    private val minIOService: MinIOService
+) {
+
+    private companion object : KLogging()
+
+    /**
+     * Запрос файлов из файлового хранилища
+     * @param - request
+     */
+    fun retrieveFiles(folderName: String, fileName: String): File {
+        logger.info { "Запрос файлов из файлового хранилища" }
+        val file = File(fileName)
+        FileUtils.copyInputStreamToFile(
+            minIOService.getFile(
+                "bucketName", String.format("%s/%s", folderName, fileName)
+            ), file
+        )
+        return file
+    }
+
+    /**
+     * Сохранение файла в файловом хранилище и получение ссылки на файл
+     * @param - request
+     */
+    fun saveFile(file: MultipartFile): String {
+        logger.info { "Загрузка файла в файловое хранилище" }
+        val folderName: String = UUID.randomUUID().toString()
+        val fileName: String = file.name
+        return minIOService.saveFile("bucketName", String.format("%s/%s", folderName, fileName), file)
+    }
+}
+```
+
+### Запуск MinIO в Docker
+
+```yaml
+version: '3.7'
+
+services:
+  minio:
+    image: minio/minio:latest
+    command: server --console-address ":9001" /data/
+    ports:
+      - "9000:9000"
+      - "9001:9001"
+    environment:
+      MINIO_ROOT_USER: MINIO_ACCESS_KEY
+      MINIO_ROOT_PASSWORD: MINIO_SECRET_KEY
+    volumes:
+      - minio-storage:/data
+    healthcheck:
+      test: [ "CMD", "curl", "-f", "http://localhost:9000/minio/health/live" ]
+      interval: 30s
+      timeout: 20s
+      retries: 3
+volumes:
+  minio-storage:
+```
--- src/main/java/ru/dnx/spring/boot/starter/minio/MinIOService.java
+++ src/main/java/ru/dnx/spring/boot/starter/minio/MinIOService.java
@@ -1,12 +1,17 @@
 package ru.dnx.spring.boot.starter.minio;
 
 import io.minio.MinioClient;
+import io.minio.Result;
+import io.minio.messages.Bucket;
+import io.minio.messages.Item;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Сервис для взаимодействия с MinIO Client
@@ -18,6 +23,20 @@ public class MinIOService {
     private final MinioClient minioClient;
 
     /**
+     * Получение всех файлов в хранилище
+     *
+     * @param bucketName - наименование бакета в хранилище
+     * @return список всех файлов в хранилище
+     */
+    @SneakyThrows
+    public Iterable<Result<Item>> getFiles(String bucketName) {
+        log.info("Получение всех файлов в хранилище");
+        return minioClient.listObjects(bucketName);
+    }
+
+    /**
+     * Получение файла из хранилища
+     *
      * @param bucketName - наименование бакета в хранилище
      * @param filePath   - наименование пути до файла в хранилище
      * @return поток байтов
@@ -45,6 +64,39 @@ public class MinIOService {
     }
 
     /**
+     * Сохрание файла в хранилище
+     *
+     * @param bucketName  - наименование бакета в хранилище
+     * @param filePath    - наименование пути до файла в хранилище
+     * @param contentType - тип передаемого контента
+     * @param inputStream - входящий поток
+     * @return ссылка на файл в хранилище
+     */
+    @SneakyThrows
+    public String saveFile(String bucketName, String filePath, String contentType, InputStream inputStream) {
+        log.info("Проверка наличия бакета");
+        makeBucket(bucketName);
+        log.info("Сохрание файла в хранилище");
+        minioClient.putObject(bucketName, filePath, inputStream, inputStream.available(), contentType);
+        return filePath;
+    }
+
+    /**
+     * Копирование файла в хранилище
+     *
+     * @param bucketName   - наименование бакета в хранилище
+     * @param filePath     - наименование пути до файла в хранилище
+     * @param destFilePath - наименование пути для копирования файла в хранилище
+     */
+    @SneakyThrows
+    public void copyFile(String bucketName, String filePath, String destFilePath) {
+        log.info("Проверка наличия бакета");
+        makeBucket(bucketName);
+        log.info("Сохрание файла в хранилище");
+        minioClient.copyObject(bucketName, filePath, destFilePath);
+    }
+
+    /**
      * Удаление файла в хранилище
      *
      * @param bucketName - наименование бакета в хранилище
@@ -52,6 +104,7 @@ public class MinIOService {
      */
     @SneakyThrows
     public void removeFile(String bucketName, String filePath) {
+        log.info("Удаление файла в хранилище");
         minioClient.removeObject(bucketName, filePath);
     }
 
@@ -63,8 +116,20 @@ public class MinIOService {
     @SneakyThrows
     public void makeBucket(String bucketName) {
         if (!bucketExists(bucketName)) {
+            log.info("Создание бакета в хранилище");
             minioClient.makeBucket(bucketName);
         }
+    }
+
+    /**
+     * Получение списка наименований всех бакетов в хранилище
+     *
+     * @return список наименований всех бакетов в хранилище
+     */
+    @SneakyThrows
+    public List<String> getBuckets() {
+        log.info("Получение списка наименований всех бакетов в хранилище");
+        return minioClient.listBuckets().stream().map(Bucket::name).collect(Collectors.toList());
     }
 
     /**
@@ -75,6 +140,7 @@ public class MinIOService {
     @SneakyThrows
     public void removeBucket(String bucketName) {
         if (bucketExists(bucketName)) {
+            log.info("Удаление бакета в хранилище");
             minioClient.removeBucket(bucketName);
         }
     }
@@ -87,6 +153,7 @@ public class MinIOService {
      */
     @SneakyThrows
     private boolean bucketExists(String bucketName) {
+        log.info("Проверка наличия бакета в хранилище");
         return minioClient.bucketExists(bucketName);
     }
 }
